6a8965a6f4b06e8dc0c2d035153271f7fd446a43
komo97 <jose.m.perez@saei.mx>
2017-11-29 20:08:15 -0600

    YA FUNCIONA
    Corregido y comentado toda la api, solo es cuestion de compilar en plataformas x64 y x86 y montarlo sobre unity.

---
+++ FlexBisonUnityDll/FlexBisonUnityDll/FlexBisonUnityDll.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <fstream>
+#include <chrono>
+#include <string>
+#include <iostream>
+
+extern "C++"
+{
+
+	typedef struct String
+	{
+		char* str;
+		size_t len;
+	}FlexString, BisonString;
+
+	/**********************************************************************************************//**
+	 * \fn	_declspec(dllexport) void __cdecl BisonExecution();
+	 *
+	 * \brief	Esta es la funcion principal de ejecución del intepretador, no se va a llamar en
+	 *			unity pues se llama internamente al inicializarla.
+	 *
+	 *
+	 **************************************************************************************************/
+	_declspec(dllexport) void __cdecl BisonExecution();
+	/**********************************************************************************************//**
+	 * \fn	_declspec(dllexport) BisonString __cdecl GetBisonString();
+	 *
+	 * \brief	Regresa el BisonString mas antiguo que se encuentre en el queue de strings. Se espera
+	 *			a que el queue tenga al menos un dato dentro de el para regresar datos, pero se
+	 *			ejecuta en un thread diferente al principal y al de bison.
+	 *
+	 * \return	Asegurado que va a regresar un BisonString.
+	 **************************************************************************************************/
+	_declspec(dllexport) BisonString __cdecl GetBisonString();
+	/**********************************************************************************************//**
+	 * \fn	_declspec(dllexport) void __cdecl BisonOut(std::string out);
+	 *
+	 * \brief	Para usarse dentro de los archivos de bison y flex, toma el output que le den
+	 *			y lo agrega al queue de salida para GetBisonString();
+	 *
+	 * \param [in]	out 	El string que se vaya a meter al queue.
+	 *
+	 **************************************************************************************************/
+	_declspec(dllexport) void __cdecl BisonOut(std::string out);
+	/**********************************************************************************************//**
+	 * \fn	_declspec(dllexport) void __cdecl PushBisonString(char* str, const size_t& len);
+	 *
+	 * \brief	Mete un string de cualquier lado al analizador de bison, es la unica interfaz
+	 *			de entrada para la api. Se le van a agregar un '\n' y  2 '\0' al final del string.
+	 *			Esto es por especificación de lex.
+	 *
+	 * \param [in]	str		El string que vaya a meterse.
+	 * \param [in]	len		El tamaño del string.
+	 *
+	 **************************************************************************************************/
+	_declspec(dllexport) void __cdecl PushBisonString(char* str, const size_t& len);
+	/**********************************************************************************************//**
+	 * \fn	_declspec(dllexport) void __cdecl StartBison();
+	 *
+	 * \brief	Esta funcion inicia el parser lexico con su thread, es importante que se llame al
+	 *			inicio o todo lo que se llame subsecuentemente va a tirar null.
+	 *
+	 **************************************************************************************************/
+	_declspec(dllexport) void __cdecl StartBison();
+	/**********************************************************************************************//**
+	 * \fn	_declspec(dllexport) void __cdecl TerminateBison();
+	 *
+	 * \brief	Llamar a esta funcion para terminar el interpretador, es importante llamarla para
+	 *			evitar memory leaks.
+	 *
+	 **************************************************************************************************/
+	_declspec(dllexport) void __cdecl TerminateBison();
+};
--- FlexBisonUnityDll/FlexBisonUnityDll/calc.y
+++ FlexBisonUnityDll/FlexBisonUnityDll/calc.y
@@ -1,42 +1,42 @@
+
+
+/*IMPORTANTE*/
+%define api.pure full
+/*IMPORTANTE*/
+%lex-param {void * scanner}
+/*IMPORTANTE*/
+%parse-param {void * scanner}
+
 %{
 #include <stdio.h>
+/*IMPORTANTE*/
+#include <string>
+/*IMPORTANTE: SUSTITUIR CALC POR EL NOMBRE DEL ARCHIVO .y*/
+#include "calc.tab.h"
+/*IMPORTANTE: SUSTITUIR CALC POR EL NOMBRE DEL ARCHIVO .l*/
 #include "calc.flex.h"
-
-int kodigo=0;
-
-int yywrap(void)
-{
-return 0;
-}
-void yyerror(const char *)
+/*IMPORTANTE*/
+#include "FlexBisonUnityDll.h"
+/*IMPORTANTE*/
+void yyerror(yyscan_t scanner, char const *msg)
 {
 }
 %}
-%token arriba izquierda abajo derecha
-%token coma
-%token EOL
-%%
-exp: arriba coma arriba coma abajo coma EOL {kodigo=1;}
-;
-%%
+/*IMPORTANTE*/
+%code requires
+{
+	typedef int YYSTYPE;
+}
 
-
-
-
-
-
-
-
-
-/*
 %token NUMBER
 %token ADD SUB MUL DIV
 %token OP CP
 %token EOL
-
+	
 %%
 calclist: 
-| calclist exp EOL {printf("= %d\n", $2);}
+| calclist exp EOL {std::string out = std::to_string($2);
+BisonOut(out);}
 ;
 exp: factor
 | exp ADD factor { $$ = $1 + $3; }
@@ -50,19 +50,7 @@ term: NUMBER
 | OP exp CP { $$ = $2; }
 ;
 %%
-
-*/
-
-
-	/*
-	You call the function yyparse to cause parsing to occur. This function reads tokens, executes actions, and ultimately returns when it encounters end-of-input or an unrecoverable syntax error. You can also write an action which directs yyparse to return immediately without reading further.
-
-Function: int yyparse (void)
-The value returned by yyparse is 0 if parsing was successful (return is due to end-of-input).
-
-The value is 1 if parsing failed because of invalid input, i.e., input that contains a syntax error or that causes YYABORT to be invoked.
-
-The value is 2 if parsing failed due to memory exhaustion.
-
-In an action, you can cause immediate return from yyparse by using these macros:
+/*CODIGO DE EJEMPLO
+PARA HACER OTRO HAY QUE UTILIZAR LO QUE SE REFIERA AQUI COMO IMPORTANTE
+Y POR UTILIZAR ME REFIERO A COPIAR Y PEGAR DIRECTAMENTE
 */
